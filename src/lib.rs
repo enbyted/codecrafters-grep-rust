f95450ea4aa1eb6ed636b3883370456e9615d60e
Bartosz Grabias <bartosz.grabias@enbyted.pl>
2023-12-24 19:44:27 +0000

    Match digits

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,46 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum Error {}
+pub enum Error {
+    #[error("Unexpected end of input")]
+    EOF,
+    #[error("Unknown character group `\\{0}`")]
+    UnknownCharacterType(char),
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 enum Matcher {
     Literal(char),
+    Digit,
 }
 
 impl Matcher {
+    pub fn new(input: &mut impl Iterator<Item = char>) -> Result<Self> {
+        match input.next() {
+            Some('\\') => match input.next() {
+                Some('\\') => Ok(Self::Literal('\\')),
+                Some('d') => Ok(Self::Digit),
+                Some(ch) => Err(Error::UnknownCharacterType(ch)),
+                None => Err(Error::EOF),
+            },
+            Some(ch) => Ok(Self::Literal(ch)),
+            None => Err(Error::EOF),
+        }
+    }
+
     pub fn test(&self, input: &mut impl Iterator<Item = char>) -> bool {
         match self {
             Matcher::Literal(c) => {
                 if let Some(ch) = input.next() {
                     ch == *c
+                } else {
+                    false
+                }
+            }
+            Matcher::Digit => {
+                if let Some(ch) = input.next() {
+                    ch.is_ascii_digit()
                 } else {
                     false
                 }
@@ -29,7 +55,11 @@ pub struct Pattern {
 
 impl Pattern {
     pub fn new(input: &str) -> Result<Self> {
-        let matchers = input.chars().map(|c| Matcher::Literal(c)).collect();
+        let mut input = input.chars().peekable();
+        let mut matchers = Vec::new();
+        while let Some(_) = input.peek() {
+            matchers.push(Matcher::new(&mut input)?);
+        }
 
         Ok(Self { matchers })
     }
@@ -82,5 +112,23 @@ mod test {
         assert!(pattern.test("012abcde"));
         assert!(!pattern.test("lax"));
         assert!(!pattern.test("abxc"));
+    }
+
+    #[test]
+    fn digit_match() {
+        let pattern = Pattern::new(r"\d").expect("Pattern is correct");
+        assert!(pattern.test("1"));
+        assert!(pattern.test("a2"));
+        assert!(pattern.test("012abcde"));
+        assert!(!pattern.test("lax"));
+        assert!(!pattern.test("abxc"));
+    }
+
+    #[test]
+    fn full_test() {
+        let pattern = Pattern::new(r"a\dc").expect("Pattern is correct");
+        assert!(pattern.test("a9c"));
+        assert!(pattern.test("da4cg"));
+        assert!(!pattern.test("ab9c"));
     }
 }
