6ea220edae5d0b2447f891765b97a7c53f5ee016
Bartosz Grabias <bartosz.grabias@enbyted.pl>
2025-11-08 19:13:39 +0100

    Update language pack and support grepping files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::env;
+use std::fs;
 use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Read;
 use std::process;
 
+use anyhow::Context;
 use grep_starter_rust::Pattern;
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
@@ -13,11 +18,25 @@ fn main() -> anyhow::Result<()> {
 
     let pattern = env::args().nth(2).unwrap();
     let pattern = Pattern::new(&pattern)?;
+
+    let input: Box<dyn Read> = if let Some(file) = env::args().nth(3) {
+        Box::new(fs::File::open(&file).with_context(|| format!("Failed to open file: {file}"))?)
+    } else {
+        Box::new(io::stdin())
+    };
+
+    let mut input_reader = BufReader::new(input);
     let mut input_line = String::new();
+    let mut matched = false;
+    while let Ok(len) = input_reader.read_line(&mut input_line) && len > 0 {
+        if pattern.test(&input_line) {
+            matched = true;
+            println!("{input_line}");
+        }
+        input_line.clear();
+    }
 
-    io::stdin().read_line(&mut input_line).unwrap();
-
-    if pattern.test(&input_line) {
+    if matched {
         process::exit(0)
     } else {
         process::exit(1)
