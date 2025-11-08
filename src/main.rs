a67f8951475b59d77fce4a96522540d984c19800
Bartosz Grabias <bartosz.grabias@enbyted.pl>
2025-11-08 19:35:52 +0100

    Parse -r argument

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,86 @@
-use std::env;
 use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Read;
-use std::path::Path;
 use std::path::PathBuf;
 use std::process;
 
 use anyhow::Context;
+use clap::Parser;
 use env_logger::Env;
 use grep_starter_rust::Pattern;
-use log::error;
+use log::info;
+
+#[derive(Debug, Clone, PartialEq, Eq, Parser)]
+struct ProgramArgs {
+    #[arg(short = 'E')]
+    extended: bool,
+    #[arg(short = 'r')]
+    recursive: bool,
+
+    pattern: String,
+
+    paths: Vec<PathBuf>,
+}
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() -> anyhow::Result<()> {
-    let mut args = env::args();
-    args.next();
+    let args = ProgramArgs::parse();
 
-    env_logger::init_from_env(Env::new().default_filter_or("trace"));
-    if args.next() != Some("-E".to_string()) {
-        error!("Expected first argument to be '-E'");
-        process::exit(1);
-    }
+    env_logger::init_from_env(Env::new().default_filter_or("info"));
 
-    let pattern = args.next().unwrap();
-    let pattern = Pattern::new(&pattern)?;
+    let pattern = Pattern::new(&args.pattern)?;
 
     let mut inputs: Vec<(String, Box<dyn Read>)> = vec![];
 
-    while let Some(file) = args.next() {
-        if file == "-" {
+    for file in args.paths {
+        if file.to_str() == Some("-") {
             inputs.push(("-".to_string(), Box::new(io::stdin())));
-        } else {
-            let meta = std::fs::metadata(&file)
-                .with_context(|| format!("Failed to get metadata for file: {file}"))?;
-            if meta.is_dir() {
-                let base = PathBuf::from(&file)
-                    .canonicalize()
-                    .with_context(|| format!("Failed to canonicalize path: {file}"))?;
-                let mut dir_inputs = vec![base.clone()];
-                while let Some(file) = dir_inputs.pop() {
-                    for entry in fs::read_dir(&file)
-                        .with_context(|| format!("Failed to read directory: {file:?}"))?
-                    {
-                        let entry = entry
-                            .with_context(|| format!("Failed to get entry for file in {file:?}"))?;
+        } else if file.is_dir() && args.recursive {
+            let base = PathBuf::from(&file)
+                .canonicalize()
+                .with_context(|| format!("Failed to canonicalize path: {file:?}"))?;
+            let mut dir_inputs = vec![base.clone()];
+            let base = base.parent().unwrap_or(&base);
+            while let Some(file) = dir_inputs.pop() {
+                for entry in fs::read_dir(&file)
+                    .with_context(|| format!("Failed to read directory: {file:?}"))?
+                {
+                    let entry = entry
+                        .with_context(|| format!("Failed to get entry for file in {file:?}"))?;
 
-                        if entry
-                            .file_type()
-                            .with_context(|| format!("Failed to get file type for entry {file:?}"))?
-                            .is_dir()
-                        {
-                            dir_inputs.push(entry.path());
-                        } else {
-                            inputs.push((
-                                entry
-                                    .path()
-                                    .strip_prefix(&base)
-                                    .with_context(|| {
-                                        format!(
-                                            "Failed to strip prefix from path: {:?}",
-                                            entry.path()
-                                        )
-                                    })?
-                                    .to_string_lossy()
-                                    .to_string(),
-                                Box::new(fs::File::open(&entry.path()).with_context(|| {
-                                    format!("Failed to open file: {:?}", entry.path())
-                                })?),
-                            ));
-                        }
+                    if entry
+                        .file_type()
+                        .with_context(|| format!("Failed to get file type for entry {file:?}"))?
+                        .is_dir()
+                    {
+                        dir_inputs.push(entry.path());
+                    } else {
+                        inputs.push((
+                            entry
+                                .path()
+                                .strip_prefix(&base)
+                                .with_context(|| {
+                                    format!("Failed to strip prefix from path: {:?}", entry.path())
+                                })?
+                                .to_string_lossy()
+                                .to_string(),
+                            Box::new(fs::File::open(&entry.path()).with_context(|| {
+                                format!("Failed to open file: {:?}", entry.path())
+                            })?),
+                        ));
                     }
                 }
-            } else {
-                inputs.push((
-                    file.clone(),
-                    Box::new(
-                        fs::File::open(&file)
-                            .with_context(|| format!("Failed to open file: {file}"))?,
-                    ),
-                ));
             }
+        } else {
+            inputs.push((
+                file.to_string_lossy().to_string(),
+                Box::new(
+                    fs::File::open(&file)
+                        .with_context(|| format!("Failed to open file: {file:?}"))?,
+                ),
+            ));
         }
     }
 
@@ -94,6 +93,7 @@ fn main() -> anyhow::Result<()> {
     let has_multiple_inputs = inputs.len() > 1;
 
     for (name, input) in inputs {
+        info!("Processing {name:?}");
         let mut input_reader = BufReader::new(input);
         while let Ok(len) = input_reader.read_line(&mut input_line)
             && len > 0
@@ -103,7 +103,7 @@ fn main() -> anyhow::Result<()> {
                 if has_multiple_inputs {
                     print!("{name}:");
                 }
-                print!("{input_line}");
+                println!("{}", input_line.trim_end_matches('\n'));
             }
             input_line.clear();
         }
