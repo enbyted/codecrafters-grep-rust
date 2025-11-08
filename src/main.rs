6ab002cd6f88b70301eedb45baea246ec19b706b
Bartosz Grabias <bartosz.grabias@enbyted.pl>
2025-11-08 19:23:17 +0100

    Support mutiple files

--- src/main.rs
+++ src/main.rs
@@ -13,32 +13,56 @@ use log::error;
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() -> anyhow::Result<()> {
+    let mut args = env::args();
+    args.next();
+
     env_logger::init_from_env(Env::new().default_filter_or("trace"));
-    if env::args().nth(1).unwrap() != "-E" {
+    if args.next() != Some("-E".to_string()) {
         error!("Expected first argument to be '-E'");
         process::exit(1);
     }
 
-    let pattern = env::args().nth(2).unwrap();
+    let pattern = args.next().unwrap();
     let pattern = Pattern::new(&pattern)?;
 
-    let input: Box<dyn Read> = if let Some(file) = env::args().nth(3) {
-        Box::new(fs::File::open(&file).with_context(|| format!("Failed to open file: {file}"))?)
-    } else {
-        Box::new(io::stdin())
-    };
+    let mut inputs: Vec<(String, Box<dyn Read>)> = vec![];
 
-    let mut input_reader = BufReader::new(input);
+    while let Some(file) = args.next() {
+        if file == "-" {
+            inputs.push(("-".to_string(), Box::new(io::stdin())));
+        } else {
+            inputs.push((
+                file.clone(),
+                Box::new(
+                    fs::File::open(&file)
+                        .with_context(|| format!("Failed to open file: {file}"))?,
+                ),
+            ));
+        }
+    }
+
+    if inputs.is_empty() {
+        inputs.push(("-".to_string(), Box::new(io::stdin())));
+    }
+
     let mut input_line = String::new();
     let mut matched = false;
-    while let Ok(len) = input_reader.read_line(&mut input_line)
-        && len > 0
-    {
-        if pattern.test(&input_line) {
-            matched = true;
-            print!("{input_line}");
+    let has_multiple_inputs = inputs.len() > 1;
+
+    for (name, input) in inputs {
+        let mut input_reader = BufReader::new(input);
+        while let Ok(len) = input_reader.read_line(&mut input_line)
+            && len > 0
+        {
+            if pattern.test(&input_line) {
+                matched = true;
+                if has_multiple_inputs {
+                    print!("{name}:");
+                }
+                print!("{input_line}");
+            }
+            input_line.clear();
         }
-        input_line.clear();
     }
 
     if matched {
